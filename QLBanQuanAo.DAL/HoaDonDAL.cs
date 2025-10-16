5152d65fb7556b2d0b69aa6fe2ff889b49847a2f
Dao Duy Duc <daoduyduc2005bh@gmail.com>
2025-10-16 14:27:51 +0700

    hoadon1

---
+++ QLBanQuanAo.DAL/HoaDonDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QLBanQuanAo.DAL
+{
+    public class HoaDonDAL
+    {
+        private string connectionString = @"Data Source=.;Initial Catalog=QLBanQuanAo;Integrated Security=True";
+
+        public DataTable LayDanhSachHoaDon()
+        {
+            string query = "SELECT MaHD, MaKH, Ma, NgayLap, TongTien, TrangThai FROM HoaDon";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public DataTable TimKiemHoaDon(string maHD, DateTime? ngayLap)
+        {
+            string query = "SELECT MaHD, MaKH, Ma, NgayLap, TongTien, TrangThai FROM HoaDon WHERE 1=1";
+
+            if (!string.IsNullOrEmpty(maHD))
+                query += " AND MaHD = @MaHD";
+
+            if (ngayLap.HasValue)
+                query += " AND CAST(NgayLap AS DATE) = @NgayLap";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (!string.IsNullOrEmpty(maHD))
+                    cmd.Parameters.AddWithValue("@MaHD", maHD);
+                if (ngayLap.HasValue)
+                    cmd.Parameters.AddWithValue("@NgayLap", ngayLap.Value.Date);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
--- QLBanQuanAo/frmHoaDon.cs
+++ QLBanQuanAo/frmHoaDon.cs
@@ -1,23 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLBanQuanAo.BUS;
 
-namespace QLBanQuanAo
+namespace QLBanQuanAo.UI
 {
     public partial class frmHoaDon : Form
     {
+        private HoaDonBUS hoaDonBUS = new HoaDonBUS();
+
         public frmHoaDon()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void frmHoaDon_Load(object sender, EventArgs e)
+        {
+            dgvHoaDon.DataSource = hoaDonBUS.LayDanhSachHoaDon();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string maHD = txtMaHD.Text.Trim();
+            DateTime? ngayLap = dtpNgayLap.Checked ? dtpNgayLap.Value : (DateTime?)null;
+
+            DataTable dt = hoaDonBUS.TimKiemHoaDon(maHD, ngayLap);
+            dgvHoaDon.DataSource = dt;
+        }
+
+        private void btnTaiLai_Click(object sender, EventArgs e)
+        {
+            txtMaHD.Clear();
+            dtpNgayLap.Checked = false;
+            dgvHoaDon.DataSource = hoaDonBUS.LayDanhSachHoaDon();
+        }
+
+        private void lblNgayLap_Click(object sender, EventArgs e)
         {
 
         }
