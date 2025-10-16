8313cd4610d3fd38da8ea016c3f1a26e3c95568e
Dao Duy Duc <daoduyduc2005bh@gmail.com>
2025-10-16 13:30:05 +0700

    register2

--- QLBanQuanAo/frmRegister.Designer.cs
+++ QLBanQuanAo/frmRegister.Designer.cs
@@ -1,39 +1,129 @@
-﻿namespace QLBanQuanAo
+﻿namespace QuanLyBanVe
 {
-    partial class frmRegister
+    partial class FormRegister
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTenDangNhap;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.Label lblXacNhan;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtXacNhan;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Button btnQuayLai;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmRegister";
+            this.lblTenDangNhap = new System.Windows.Forms.Label();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.lblXacNhan = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtXacNhan = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.btnQuayLai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTenDangNhap
+            // 
+            this.lblTenDangNhap.AutoSize = true;
+            this.lblTenDangNhap.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblTenDangNhap.Location = new System.Drawing.Point(40, 40);
+            this.lblTenDangNhap.Text = "Tên đăng nhập:";
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblHoTen.Location = new System.Drawing.Point(40, 90);
+            this.lblHoTen.Text = "Họ tên:";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblMatKhau.Location = new System.Drawing.Point(40, 140);
+            this.lblMatKhau.Text = "Mật khẩu:";
+            // 
+            // lblXacNhan
+            // 
+            this.lblXacNhan.AutoSize = true;
+            this.lblXacNhan.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblXacNhan.Location = new System.Drawing.Point(40, 190);
+            this.lblXacNhan.Text = "Xác nhận mật khẩu:";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtTenDangNhap.Location = new System.Drawing.Point(200, 40);
+            this.txtTenDangNhap.Width = 220;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtHoTen.Location = new System.Drawing.Point(200, 90);
+            this.txtHoTen.Width = 220;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtMatKhau.Location = new System.Drawing.Point(200, 140);
+            this.txtMatKhau.Width = 220;
+            this.txtMatKhau.PasswordChar = '*';
+            // 
+            // txtXacNhan
+            // 
+            this.txtXacNhan.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtXacNhan.Location = new System.Drawing.Point(200, 190);
+            this.txtXacNhan.Width = 220;
+            this.txtXacNhan.PasswordChar = '*';
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnDangKy.Location = new System.Drawing.Point(120, 250);
+            this.btnDangKy.Size = new System.Drawing.Size(100, 35);
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // btnQuayLai
+            // 
+            this.btnQuayLai.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnQuayLai.Location = new System.Drawing.Point(260, 250);
+            this.btnQuayLai.Size = new System.Drawing.Size(120, 35);
+            this.btnQuayLai.Text = "Quay lại đăng nhập";
+            this.btnQuayLai.Click += new System.EventHandler(this.btnQuayLai_Click);
+            // 
+            // FormRegister
+            // 
+            this.ClientSize = new System.Drawing.Size(480, 320);
+            this.Controls.Add(this.lblTenDangNhap);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblXacNhan);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtXacNhan);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnQuayLai);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng ký tài khoản";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
         #endregion
     }
 }
--- QLBanQuanAo/frmRegister.cs
+++ QLBanQuanAo/frmRegister.cs
@@ -1,20 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QLBanQuanAo;
+using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace QLBanQuanAo
+namespace QuanLyBanVe
 {
-    public partial class frmRegister : Form
+    public partial class FormRegister : Form
     {
-        public frmRegister()
+        string connectionString = @"Data Source=.;Initial Catalog=QuanLyBanVe;Integrated Security=True";
+
+        public FormRegister()
         {
             InitializeComponent();
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            string tenDangNhap = txtTenDangNhap.Text.Trim();
+            string hoTen = txtHoTen.Text.Trim();
+            string matKhau = txtMatKhau.Text;
+            string xacNhan = txtXacNhan.Text;
+
+            if (tenDangNhap == "" || hoTen == "" || matKhau == "" || xacNhan == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (matKhau != xacNhan)
+            {
+                MessageBox.Show("Mật khẩu xác nhận không khớp!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string queryCheck = "SELECT COUNT(*) FROM Account WHERE TenDangNhap = @TenDangNhap";
+                    SqlCommand cmdCheck = new SqlCommand(queryCheck, conn);
+                    cmdCheck.Parameters.AddWithValue("@TenDangNhap", tenDangNhap);
+                    int count = (int)cmdCheck.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Tên đăng nhập đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    string query = "INSERT INTO Account (TenDangNhap, MatKhau, HoTen) VALUES (@TenDangNhap, @MatKhau, @HoTen)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@TenDangNhap", tenDangNhap);
+                    cmd.Parameters.AddWithValue("@MatKhau", matKhau);
+                    cmd.Parameters.AddWithValue("@HoTen", hoTen);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Đăng ký thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    frmLogin f = new frmLogin();
+                    f.Show();
+                    this.Hide();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnQuayLai_Click(object sender, EventArgs e)
+        {
+            frmLogin f = new frmLogin();
+            f.Show();
+            this.Hide();
         }
     }
 }
